a9de0f8a6f5580f6dc6eb8da157d26c128f350ed
Bojan Mirkovic <bojanmirkovic81@gmail.com>
2025-01-16 12:37:01 +0100

    I have instal neccessary Nuget packages and I have crete OrderModel in Domain layer

---
+++ OrderApi.Domain/EntityModels/OrderModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace OrderApi.Domain.EntityModels
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+        public int PurchaseQuantity { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    }
+}
